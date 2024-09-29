7efd30e2ebce31d8b2519c1cc4a96ba352286adb
JouTiago <ajourneya@gmail.com>
2024-09-29 18:04:58 -0300

    Adição dos botões de modal da direita

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { useState } from 'react';
 import './assets/styles.css';
-import SkinCareModal from './components/SkinCareModal';
-import Timer from './components/Clock';
+import SkinCareModal from './components/modals/SkinCareModal';
+import Timer from './components/modals/Clock';
+import ModalButtonsRight from './components/ModalButtons';
 
 const App = () => {
-  const [isSkinCareOpen, setIsSkinCareOpen] = useState(false);
+  const [activeModal, setActiveModal] = useState<string | null>(null);
+
+  const handleButtonClick = (modalName: string) => {
+    setActiveModal(modalName);
+  };
+
+  const closeModal = () => setActiveModal(null);
+
 
   return (
-    <div className="app-container">
-      <Timer />
-      <button className="open-modal-btn" onClick={() => setIsSkinCareOpen(true)}>
-        Open Skin Care Modal
-      </button>
 
-      {/* Modal */}
-      <SkinCareModal isOpen={isSkinCareOpen} onClose={() => setIsSkinCareOpen(false)} />
+    <div className="app-container">      
+
+      <Timer />
+
+      <div className='modal-container'>
+        <div className='modal-box'>left</div>
+      
+        <ModalButtonsRight onButtonClick={handleButtonClick} />
+      </div>
+
+      {activeModal === 'SkinCareModal' && <SkinCareModal isOpen={true} onClose={closeModal} />}
+    
     </div>
   );
 };
